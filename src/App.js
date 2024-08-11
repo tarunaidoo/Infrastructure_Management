275ed95381cb8b733472aeeda15a211d3251385d
Shaneel <2539340@students.wits.ac.za>
2024-08-11 09:38:27 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+async function list() {
+  const endpoint = '/data-api/rest/Person';
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.table(data.value);
+}
+
+
 function App() {
+  
+
   const value = 'World';
+  list();
   return <div>Hello and a aa{value}</div>;
 }
 
