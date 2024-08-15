2e7a71d0a1ff7bf2ff8f3c6eac1733b3cc397ada
Shaneel <2539340@students.wits.ac.za>
2024-08-15 14:19:46 +0200

    Added 3rd party verification

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LoginButton from './LoginButton.js';
+import LogoutButton from './LogoutButton.js';
+import Profile from './Profile.js';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-db0zyxrziyqogi48.us.auth0.com"
+    clientId="WztTTE9jAlLWYlL2tDLexMRbKdRWBSGk"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+    <LoginButton />
+    
+    <Profile />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
