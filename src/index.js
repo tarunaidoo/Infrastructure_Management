47bacfd16eb5ccb4b45a3163572ab2743b395edf
YassirAwad <2623035@students.wits.ac.za>
2024-09-10 00:57:31 +0200

    -Removed Unused libraries

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <Auth0Provider
@@ -15,9 +13,9 @@ ReactDOM.render(
       redirect_uri: window.location.origin,
     }}
   >
-  <QueryClientProvider client={queryClient}>
+
     <App />
-  </QueryClientProvider>,
+
   </Auth0Provider>,
   document.getElementById('root')
 );
