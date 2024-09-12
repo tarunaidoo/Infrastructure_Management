8678753227a87e4c5d8793da2c03ca230e260d78
DEGEN101 <ntando.raji13@gmail.com>
2024-09-12 09:01:55 +0200

    Created edit booking page directory with basic files

---
+++ src/pages/EditBookingPage/EditBookingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const EditBookingPage = () => {
+    return (
+        <>
+            <h1> Booking Page </h1>
+        </>
+    );
+}
+
+export default EditBookingPage;
