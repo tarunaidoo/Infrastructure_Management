aabe6cd748f5a079c81428e24077e6c861e3961b
Tania908 <2486457@students.wits.ac.za>
2024-09-19 18:46:03 +0200

    Another Commit

--- src/components/CampusCard/CampusCard.jsx
+++ src/components/CampusCard/CampusCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import pointer from "../../assets/icons/crosshairs.svg"
 import "./CampusCard.css"
 
 
 const CampusCard = ({campusName, onClick}) => {
     return (
         <section data-testid="campus-card" className="campus-card-container" onClick={onClick}>
+            <img src={pointer} alt="pointerIcon"/>
             <p data-testid="campus-card-text"className="campus-name-text">{campusName}</p>
         </section>
     );
