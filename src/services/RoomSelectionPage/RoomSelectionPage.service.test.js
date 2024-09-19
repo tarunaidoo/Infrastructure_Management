fb9e06b33ded5b5a88a3f2c2c6f64b59d2d90c87
DEGEN101 <ntando.raji13@gmail.com>
2024-09-19 21:44:31 +0200

    Created API tests for newly created API funtion calls

--- src/services/RoomSelectionPage/RoomSelectionPage.service.test.js
+++ src/services/RoomSelectionPage/RoomSelectionPage.service.test.js
@@ -1,4 +1,4 @@
-import { getVenuesFromBuildingID, getVenueFeatureIDFromVenueID } from "./RoomSelectionPage.service";
+import { getRoleFromUserID, getVenuesFromBuildingIDAndUserID, getVenueFeatureIDFromVenueID } from "./RoomSelectionPage.service";
 
 // Mock the fetch API
 global.fetch = jest.fn();
@@ -8,15 +8,59 @@ describe("Room Selection Page Services", () => {
         jest.resetAllMocks();
     });
 
-    it("should return correct rooms from the building ID", async () => {
+    it("should return admin role from user ID", async () => {
+        const mockUserData = {
+            value: [
+                {USER_ID: 1, USER_ROLE: "Student"},
+                {USER_ID: 2, USER_ROLE: "Admin"}
+            ],
+        };
+
+        fetch.mockResolvedValueOnce({
+            json: jest.fn().mockResolvedValueOnce(mockUserData),
+        });
+
+        const userID = 2;
+        const result = await getRoleFromUserID(userID);
+
+        // Check if the function returns only the admin role from admin user ID
+        expect(result).toEqual("Admin");
+
+        // Ensure the fetch was called with the correct endpoint
+        expect(fetch).toHaveBeenCalledWith("/data-api/rest/USERS");
+    })
+
+    it("should return student role from user ID", async () => {
+        const mockUserData = {
+            value: [
+                {USER_ID: 1, USER_ROLE: "Student"},
+                {USER_ID: 2, USER_ROLE: "Admin"}
+            ],
+        };
+
+        fetch.mockResolvedValueOnce({
+            json: jest.fn().mockResolvedValueOnce(mockUserData),
+        });
+
+        const userID = 1;
+        const result = await getRoleFromUserID(userID);
+
+        // Check if the function returns only the student role from student user ID
+        expect(result).toEqual("Student");
+
+        // Ensure the fetch was called with the correct endpoint
+        expect(fetch).toHaveBeenCalledWith("/data-api/rest/USERS");
+    })
+
+    it("should return correct rooms from the building ID & Admin Role", async () => {
         // Mock data that the fetch API will return
         const mockBuildingsData = {
             value: [
-                {VENUE_ID: 1, BUILDING_ID: 1, VENUE_NAME:"MSL004", VENUE_STATUS: "Available"},
+                {VENUE_ID: 1, BUILDING_ID: 1, VENUE_NAME:"MSL004", VENUE_STATUS: "Unavailable"},
                 {VENUE_ID: 2, BUILDING_ID: 1, VENUE_NAME:"MSL005", VENUE_STATUS: "Available"},
                 {VENUE_ID: 3, BUILDING_ID: 1, VENUE_NAME:"MSL006", VENUE_STATUS: "Available"},
-                {VENUE_ID: 4, BUILDING_ID: 2, VENUE_NAME:"WSS113", VENUE_STATUS: "Available"},
-                {VENUE_ID: 5, BUILDING_ID: 2, VENUE_NAME:"WSS111", VENUE_STATUS: "Available"},
+                {VENUE_ID: 4, BUILDING_ID: 2, VENUE_NAME:"WSS113", VENUE_STATUS: "Unavailable"},
+                {VENUE_ID: 5, BUILDING_ID: 2, VENUE_NAME:"WSS111", VENUE_STATUS: "Unavailable"},
                 {VENUE_ID: 6, BUILDING_ID: 2, VENUE_NAME:"WSS112", VENUE_STATUS: "Available"}
             ],
         };
@@ -28,12 +72,46 @@ describe("Room Selection Page Services", () => {
 
         // Call the function and pass the campus you want to filter
         const buildingID = 2;
-        const result = await getVenuesFromBuildingID(buildingID);
+        const userRole = "Admin";
+        const result = await getVenuesFromBuildingIDAndUserID(buildingID, userRole);
+
+        // Check if the function returns only the buildings from "West Campus"
+        expect(result).toEqual([
+            {VENUE_ID: 4, BUILDING_ID: 2, VENUE_NAME:"WSS113", VENUE_STATUS: "Unavailable"},
+            {VENUE_ID: 5, BUILDING_ID: 2, VENUE_NAME:"WSS111", VENUE_STATUS: "Unavailable"},
+            {VENUE_ID: 6, BUILDING_ID: 2, VENUE_NAME:"WSS112", VENUE_STATUS: "Available"}
+        ]);
+
+        // Ensure the fetch was called with the correct endpoint
+        expect(fetch).toHaveBeenCalledWith("data-api/rest/VENUE/");
+    });
+
+    it("should return correct rooms from the building ID & Student Role", async () => {
+        // Mock data that the fetch API will return
+        const mockBuildingsData = {
+            value: [
+                {VENUE_ID: 1, BUILDING_ID: 1, VENUE_NAME:"MSL004", VENUE_STATUS: "Available"},
+                {VENUE_ID: 2, BUILDING_ID: 1, VENUE_NAME:"MSL005", VENUE_STATUS: "Available"},
+                {VENUE_ID: 3, BUILDING_ID: 1, VENUE_NAME:"MSL006", VENUE_STATUS: "Available"},
+                {VENUE_ID: 4, BUILDING_ID: 2, VENUE_NAME:"WSS113", VENUE_STATUS: "Available"},
+                {VENUE_ID: 5, BUILDING_ID: 2, VENUE_NAME:"WSS111", VENUE_STATUS: "Unavailable"},
+                {VENUE_ID: 6, BUILDING_ID: 2, VENUE_NAME:"WSS112", VENUE_STATUS: "Available"}
+            ],
+        };
+
+        // Set up the mock fetch response
+        fetch.mockResolvedValueOnce({
+            json: jest.fn().mockResolvedValueOnce(mockBuildingsData),
+        });
+
+        // Call the function and pass the campus you want to filter
+        const buildingID = 2;
+        const userRole = "Student";
+        const result = await getVenuesFromBuildingIDAndUserID(buildingID, userRole);
 
         // Check if the function returns only the buildings from "West Campus"
         expect(result).toEqual([
             {VENUE_ID: 4, BUILDING_ID: 2, VENUE_NAME:"WSS113", VENUE_STATUS: "Available"},
-            {VENUE_ID: 5, BUILDING_ID: 2, VENUE_NAME:"WSS111", VENUE_STATUS: "Available"},
             {VENUE_ID: 6, BUILDING_ID: 2, VENUE_NAME:"WSS112", VENUE_STATUS: "Available"}
         ]);
 
