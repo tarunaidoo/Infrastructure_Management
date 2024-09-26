96309c90575efad444ea6ee32baa95b69930b6ec
Tania908 <2486457@students.wits.ac.za>
2024-09-26 13:56:09 +0200

    More CSS changes and updates - Report an issue navigation changed - Report an issue saves current info when reselecting a venue - Report an issue button updated animation - Changed User Profile CSS - Added Notification label to Notification pop up - Fixed Report Issue tests

--- src/pages/ReportIssuePage/ReportIssue.jsx
+++ src/pages/ReportIssuePage/ReportIssue.jsx
@@ -4,31 +4,33 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import headingIcon from '../../assets/icons/chevron-left.svg';
 import warningIcon from '../../assets/icons/triangle-warning.svg';
 import Popup from '../../components/Popup/Popup';
+import { useQuery, useMutation, useQueryClient } from 'react-query'; // Import React Query hooks
 import { createReportIssue } from '../../services/ReportIssuePage/ReportIssuePage.service';
-import { formatDateToISO,getFormattedDate } from '../../utils/dateUtils';
+import { formatDateToISO, getFormattedDate } from '../../utils/dateUtils';
 
 
 
 function ReportIssue() {
-    const navigate = useNavigate();
-    const location = useLocation();
-    // Contain the required details from the Venue Selection Page
-    const previousPageDetails = location.state || {}; 
+  const navigate = useNavigate();
+  const location = useLocation();
+  const queryClient = useQueryClient(); // Use the query client
+  // Contain the required details from the Venue Selection Page
+  const previousPageDetails = location.state || {};
 
-    const selectedVenue = {
-      USER_ID: previousPageDetails.USER_ID,
-      BUILDING_ID: previousPageDetails.BUILDING_ID,
-      BUILDING_NAME: previousPageDetails.BUILDING_NAME,
-      VENUE_ID: previousPageDetails.VENUE_ID,
-      VENUE_NAME: previousPageDetails.VENUE_NAME
-    }
+  const selectedVenue = {
+    USER_ID: previousPageDetails.USER_ID,
+    BUILDING_ID: previousPageDetails.BUILDING_ID,
+    BUILDING_NAME: previousPageDetails.BUILDING_NAME,
+    VENUE_ID: previousPageDetails.VENUE_ID,
+    VENUE_NAME: previousPageDetails.VENUE_NAME
+  }
 
-    // Format the date i.e 24 August 2024
-    const formattedDate = getFormattedDate();
+  // Format the date i.e 24 August 2024
+  const formattedDate = getFormattedDate();
 
-    // Format the date in YYYY-MM-DD format
-    const formattedDateISO = formatDateToISO(new Date());
-  
+  // Format the date in YYYY-MM-DD format
+  const formattedDateISO = formatDateToISO(new Date());
+
 
   //dummy data
   // const venues = [
@@ -38,18 +40,37 @@ function ReportIssue() {
   // ];
   // States
 
-  const [issueTitle, setIssueTitle] = useState('');
-  const [issueDescription, setIssueDescription] = useState('');
+  // Fetch issue data from cache if it exists
+  const { data: cachedIssueData } = useQuery(
+    ['issueData'],
+    () => {
+      const issueTitle = '';
+      const issueDescription = '';
+      return { issueTitle, issueDescription };
+    },
+    {
+      initialData: {
+        issueTitle: '',
+        issueDescription: '',
+      }
+    }
+  );
+
+  const [issueTitle, setIssueTitle] = useState(cachedIssueData.issueTitle);
+  const [issueDescription, setIssueDescription] = useState(cachedIssueData.issueDescription);
   const [showPopup, setShowPopup] = useState(false);
   const [popupType, setPopupType] = useState('');
+  const [setIsLoading] = useState(false); // New state for loading
   // const [userData, setUserData] = useState(null); // State for user data
 
-
+  const updateIssueData = useMutation(
+    (newData) => queryClient.setQueryData('issueData', newData)
+  );
 
   // Handle form submission
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-  
+
     if (!issueTitle || !issueDescription) {
       setPopupType('error');
       setShowPopup(true);
@@ -64,13 +85,16 @@ function ReportIssue() {
   };
 
   const handleConfirm = () => {
+    setPopupType('loading'); // Set popup type to loading
+    setShowPopup(true);
+    setIsLoading(true); // Set loading state to true
     const reportData = {
       VENUE_ID: selectedVenue.VENUE_ID,
       TITLE: issueTitle,
       REPORTED_BY: selectedVenue.USER_ID,
       REPORT_DATE: formattedDateISO,
       DESCRIPTION: issueDescription,
-      ISSUE_STATUS:"UNRESOLVED",
+      ISSUE_STATUS: "UNRESOLVED",
     };
 
     console.log('Submitting data:', reportData);
@@ -81,16 +105,36 @@ function ReportIssue() {
     console.log(`Issue Description: ${issueDescription}`);
 
     createReportIssue(reportData)
-    .then(() => {
-      setPopupType('success');
-      setShowPopup(true);
-    })
-    .catch(() => {
-      setPopupType('request-error');
-      setShowPopup(true);
-    });
+      .then(() => {
+        setPopupType('success');
+        setIsLoading(false); // Set loading state to false
+        setShowPopup(true);
+      })
+      .catch(() => {
+        setPopupType('request-error');
+        setIsLoading(false); // Set loading state to false
+        setShowPopup(true);
+      });
   };
 
+  const handleVenueSelectionClick = () => {
+
+    // Save issue title and description to query cache before navigating
+    updateIssueData.mutate({
+      issueTitle,
+      issueDescription,
+    });
+
+    const backPageDetails = {
+      SOURCE_PAGE: previousPageDetails.SOURCE_PAGE,
+      USER_ID: previousPageDetails.USER_ID,
+      DESTINATION_PAGE: previousPageDetails.DESTINATION_PAGE,
+      CAMPUS_NAME: previousPageDetails.CAMPUS_NAME,
+      BUILDING_ID: previousPageDetails.BUILDING_ID,
+      BUILDING_NAME: previousPageDetails.BUILDING_NAME
+    }
+    navigate("/campus-selection", { state: backPageDetails });
+  }
   const handleHeaderBackIconClick = () => {
     const backPageDetails = {
       SOURCE_PAGE: previousPageDetails.SOURCE_PAGE,
@@ -100,7 +144,7 @@ function ReportIssue() {
       BUILDING_ID: previousPageDetails.BUILDING_ID,
       BUILDING_NAME: previousPageDetails.BUILDING_NAME
     }
-    navigate("/room-selection", { state: backPageDetails });
+    navigate("/student-home", { state: backPageDetails });
   }
 
 
@@ -109,8 +153,9 @@ function ReportIssue() {
     setShowPopup(false);
     if (popupType === 'success') {
       console.log("successful");
+      navigate(previousPageDetails.SOURCE_PAGE);   // Navigate to the home page 
     }
-    navigate(previousPageDetails.SOURCE_PAGE); // Navigate to the home page 
+
 
   };
 
@@ -124,92 +169,100 @@ function ReportIssue() {
 
   return (
     <main className='report-issue-layout' data-testid="report-issue-layout">
-    <article  className='report-issue-heading' data-testid="report-issue-heading">
-      <img
-      onClick={handleHeaderBackIconClick}
-        src={headingIcon}
-        alt='back-arrow'
-        className='report-icons'
-        data-testid="back-arrow-icon"
-      />
-      <h1 onClick={handleHeaderBackIconClick} data-testid="report-issue-heading-text">Report an Issue</h1>
-    </article>
-  
-    <section className='report-issue-container' data-testid="report-issue-container">
-      <form onSubmit={handleFormSubmit} data-testid="report-issue-form">
-        <p data-testid="report-issue-date">Date: {formattedDate}</p>
-        <p data-testid="report-issue-venue-label">Venue:</p>
-
-        <article data-testid="report-issue-venue-details">
-          <p data-testid="building-name">{selectedVenue.BUILDING_NAME}</p>
-          <p data-testid="venue-name">{selectedVenue.VENUE_NAME}</p>
-        </article>
-
-        <label className='issue-title-container' data-testid="issue-title-container">
-          Issue Title:
-          <input
-            type="text"
-            maxLength="50"
-            value={issueTitle}
-            onChange={(e) => setIssueTitle(e.target.value)}
-            placeholder="Enter issue title here..."
-            data-testid="issue-title-input"
+      <article className='report-issue-heading-container' data-testid="report-issue-heading">
+        <article className='report-issue-heading'>
+          <img
+            onClick={handleHeaderBackIconClick}
+            src={headingIcon}
+            alt='back-arrow'
+            className='report-icons'
+            data-testid="back-arrow-icon"
           />
-        </label>
-
-        <label className='issue-description-container' data-testid="issue-description-container">
-          Issue Description:
-          <textarea
-            value={issueDescription}
-            onChange={handleDescriptionChange}
-            maxLength="300"
-            placeholder="Describe the issue here..."
-            style={{ resize: 'none' }} // Disable resizing
-            data-testid="issue-description-input"
-          />
-        </label>
-
-        <button type="submit" data-testid="report-issue-button">
-          <img src={warningIcon} alt='warning-icon ' data-testid="warning-icon" />
-          Report Issue
-        </button>
-      </form>
-    </section>
-
-    <Popup trigger={showPopup} onClose={handleClosePopup} data-testid="popup">
-      {popupType === 'error' && (
-        <article className='report-Issue-Popups' data-testid="error-popup">
-          <h2 data-testid="error-popup-heading">Invalid Details</h2>
-          <p data-testid="error-popup-message">Please fill in all fields</p>
-          <button onClick={handleClosePopup} data-testid="error-popup-close-button">Close</button>
+          <h1 onClick={handleHeaderBackIconClick} data-testid="report-issue-heading-text">Report an Issue</h1>
         </article>
-      )}
-      {popupType === 'confirmation' && (
-        <article className='report-Issue-Popups' data-testid="confirmation-popup">
-          <h2 data-testid="confirmation-popup-heading">Confirmation</h2>
-          <p data-testid="confirmation-popup-message">Do you want to report this issue?</p>
-          <article data-testid="confirmation-popup-buttons">
-            <button onClick={handleConfirm} data-testid="confirmation-yes-button">Yes</button>
-            <button onClick={handleClosePopup} data-testid="confirmation-no-button">No</button>
+      </article>
+
+      <section className='report-issue-container' data-testid="report-issue-container">
+        <form onSubmit={handleFormSubmit} data-testid="report-issue-form">
+          <p data-testid="report-issue-date">Date: {formattedDate}</p>
+          <p data-testid="report-issue-venue-label">Venue:</p>
+
+          <article onClick={handleVenueSelectionClick} data-testid="report-issue-venue-details">
+            <p data-testid="building-name">{selectedVenue.BUILDING_NAME}</p>
+            <p data-testid="venue-name">{selectedVenue.VENUE_NAME}</p>
           </article>
-        </article>
-      )}
-      {popupType === 'success' && (
-        <article className='report-Issue-Popups' data-testid="success-popup">
-          <h2 data-testid="success-popup-heading">Confirmation</h2>
-          <p data-testid="success-popup-message">Your report has been sent</p>
-          <button onClick={handleClosePopup} data-testid="success-close-button">Close</button>
-        </article>
-      )}
-      {popupType === 'request-error' && (
-        <article className='report-Issue-Popups' data-testid="request-error-popup">
-          <h2 data-testid="request-error-popup-heading">Confirmation</h2>
-          <p data-testid="request-error-popup-message">Error Sending Request</p>
-          <button onClick={handleClosePopup} data-testid="request-error-close-button">Close</button>
-        </article>
-      )}
-    </Popup>
-  </main>
+
+          <label className='issue-title-container' data-testid="issue-title-container">
+            Issue Title:
+            <input
+              type="text"
+              maxLength="50"
+              value={issueTitle}
+              onChange={(e) => setIssueTitle(e.target.value)}
+              placeholder="Enter issue title here..."
+              data-testid="issue-title-input"
+            />
+          </label>
+
+          <label className='issue-description-container' data-testid="issue-description-container">
+            Issue Description:
+            <textarea
+              value={issueDescription}
+              onChange={handleDescriptionChange}
+              maxLength="300"
+              placeholder="Describe the issue here..."
+              style={{ resize: 'none' }} // Disable resizing
+              data-testid="issue-description-input"
+            />
+          </label>
+
+          <button className='report-issue-button' type="submit" data-testid="report-issue-button">
+            <img src={warningIcon} alt='warning-icon ' data-testid="warning-icon" />
+            Report Issue
+          </button>
+        </form>
+      </section>
+
+      <Popup trigger={showPopup} onClose={handleClosePopup} data-testid="popup">
+        {popupType === 'error' && (
+          <article className='report-Issue-Popups' data-testid="error-popup">
+            <h2 data-testid="error-popup-heading">Invalid Details</h2>
+            <p data-testid="error-popup-message">Please fill in all fields</p>
+            <button onClick={handleClosePopup} data-testid="error-popup-close-button">Close</button>
+          </article>
+        )}
+        {popupType === 'loading' && (
+          <article className='report-Issue-Popups' data-testid="loading-popup">
+            <h2 data-testid="loading-popup-heading">Loading...</h2>
+            <p data-testid="loading-popup-message">Please wait while we send your report.</p>
+          </article>
+        )}
+        {popupType === 'confirmation' && (
+          <article className='report-Issue-Popups' data-testid="confirmation-popup">
+            <h2 data-testid="confirmation-popup-heading">Confirmation</h2>
+            <p data-testid="confirmation-popup-message">Do you want to report this issue?</p>
+            <article data-testid="confirmation-popup-buttons">
+              <button onClick={handleConfirm} data-testid="confirmation-yes-button">Yes</button>
+              <button onClick={handleClosePopup} data-testid="confirmation-no-button">No</button>
+            </article>
+          </article>
+        )}
+        {popupType === 'success' && (
+          <article className='report-Issue-Popups' data-testid="success-popup">
+            <h2 data-testid="success-popup-heading">Confirmation</h2>
+            <p data-testid="success-popup-message">Your report has been sent</p>
+            <button onClick={handleClosePopup} data-testid="success-close-button">Close</button>
+          </article>
+        )}
+        {popupType === 'request-error' && (
+          <article className='report-Issue-Popups' data-testid="request-error-popup">
+            <h2 data-testid="request-error-popup-heading">Confirmation</h2>
+            <p data-testid="request-error-popup-message">Error Sending Request</p>
+            <button onClick={handleClosePopup} data-testid="request-error-close-button">Close</button>
+          </article>
+        )}
+      </Popup>
+    </main>
   );
 }
 
